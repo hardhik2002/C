d261e085981851a1982b9d2e443649c946dd4e5c
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-06-05 16:05:49 +0530

    builtit simple inttreast

---
+++ simple intreast.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float p,t,si;
+    int r;
+    scanf("%f",&p);
+    scanf("%f",&t);
+    scanf("%d",&r);
+    si=(p*t*r)/100;
+    printf("The simple intreast is:%1.f",si);
+
+
+}
